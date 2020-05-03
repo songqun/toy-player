efbc08849a1911f6b2308f4606bd8b26e43d28bd
Qun Song <songqun001@gmail.com>
2020-05-03 02:22:19 -0700

    CreateSynopsis: Update new algorithm to choose key frame

--- CreateSynopsis.java
+++ CreateSynopsis.java
@@ -1,10 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
 import javax.swing.*;
-
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.*;
 
 
 public class CreateSynopsis {
@@ -21,6 +22,7 @@ public class CreateSynopsis {
   ArrayList<String[]> frameFolderList;
   String[] imgList;
   ArrayList<String> audioList;
+  static int searchRange = 50;  // from avg-sr to avg+sr, 2*sr+1
 
   public CreateSynopsis(String folderPath, String synopsisPath)
   {
@@ -216,6 +218,121 @@ public class CreateSynopsis {
     }
   }
 
+  public void cvtImgBufferToMat(BufferedImage img, Mat imgMat)
+  {
+    int[] dataInt = img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0, img.getWidth());
+    byte[] dataByte = new byte[img.getWidth() * img.getHeight()*(int)imgMat.elemSize()];
+    for (int i = 0; i < dataInt.length; i++) {
+      dataByte[i*3] = (byte)((dataInt[i] >> 0) & 0xff);
+      dataByte[i*3+1] = (byte)((dataInt[i] >> 8) & 0xff);
+      dataByte[i*3+2] = (byte)((dataInt[i] >> 16) & 0xff);
+    }
+    imgMat.put(0, 0, dataByte);
+  }
+
+  public double getLaplacianVar(Mat imgMat)
+  {
+    Mat imgGray = new Mat();
+    Imgproc.cvtColor(imgMat, imgGray, Imgproc.COLOR_BGR2GRAY);
+    Mat imgLap = new Mat();
+    Imgproc.Laplacian(imgGray, imgLap, CvType.CV_16S);
+    MatOfDouble median = new MatOfDouble();
+    MatOfDouble std = new MatOfDouble();
+    Core.meanStdDev(imgLap, median, std);
+    double var = Math.pow(std.get(0,0)[0], 2);
+    return var;
+  }
+
+  public String[] chooseClearestImgTop10(String[] frameList)
+  {
+    TreeMap<String, Double> nameVarMap = new TreeMap<String, Double>();
+    String[] top10List = new String[10];
+    for (int i = 0; i < frameList.length; i++) {
+      BufferedImage img = new BufferedImage(iw, iw, BufferedImage.TYPE_INT_RGB);
+      readImageRGB(iw, ih, frameList[i], img);
+      Mat imgMat = new Mat(ih, iw, CvType.CV_8UC3);
+      cvtImgBufferToMat(img, imgMat);
+      double var = getLaplacianVar(imgMat);
+      nameVarMap.put(frameList[i], var);
+    }
+    ArrayList<Map.Entry<String, Double>> list = new ArrayList<Map.Entry<String, Double>>(nameVarMap.entrySet());
+    Collections.sort(list, new Comparator<Map.Entry<String, Double>>() {
+      public int compare(Map.Entry<String, Double> o1, Map.Entry<String, Double> o2) {
+        return (o2.getValue()).compareTo(o1.getValue());
+      }
+    });
+    for (int i = 0; i < 10; i++) {
+      top10List[i] = list.get(i).getKey();
+    }
+    return top10List;
+  }
+
+  public int detectFaceNum(String frameName)
+  {
+    BufferedImage img = new BufferedImage(iw, ih, BufferedImage.TYPE_INT_RGB);
+    readImageRGB(iw, ih, frameName, img);
+    Mat imgMat = new Mat(ih, iw, CvType.CV_8UC3);
+    cvtImgBufferToMat(img, imgMat);
+    Mat imgGray = new Mat();
+    Imgproc.cvtColor(imgMat, imgGray, Imgproc.COLOR_BGR2GRAY);
+    CascadeClassifier classifier = new CascadeClassifier();
+    classifier.load("lbpcascade_frontalface.xml");
+    MatOfRect faces = new MatOfRect();
+    classifier.detectMultiScale(imgGray, faces);
+    return (int)faces.total();
+  }
+
+  public void chooseSynopsisClearestTop10DetectMostFace()
+  {
+    // choose key video frame
+    int synopsisFrameNumEachVideoOrImg = synopsisFrameNum / (frameFolderList.size() + 1);
+    for (int i = 0; i < frameFolderList.size(); i++) {
+      int videoFrameNum = frameFolderList.get(i).length;
+      for (int j = 0; j < synopsisFrameNumEachVideoOrImg; j++) {
+        int avgPoint = (int)(videoFrameNum * (j+0.5) / synopsisFrameNumEachVideoOrImg);
+        String[] searchFrames = new String[searchRange*2 + 1];
+        for (int k = 0; k < 2*searchRange+1; k++) {
+          searchFrames[k] = frameFolderList.get(i)[avgPoint + k - searchRange];
+        }
+        String[] top10List = chooseClearestImgTop10(searchFrames);
+        int maxFaceNum = -1;
+        String chooseFrame = null;
+        for (int k = top10List.length-1; k >= 0; k--) {
+          int faceNum = detectFaceNum(top10List[k]);
+          if (faceNum >= maxFaceNum) {
+            maxFaceNum = faceNum;
+            chooseFrame = top10List[k];
+          }
+        }
+        outImg.metaData[i*synopsisFrameNumEachVideoOrImg + j].filePath = chooseFrame;
+        outImg.metaData[i*synopsisFrameNumEachVideoOrImg + j].isVideo = true;
+        outImg.metaData[i*synopsisFrameNumEachVideoOrImg + j].audioPath = audioList.get(i);
+      }
+    }
+    // choose key image
+    int imgNum = imgList.length;
+    String[] top10List = chooseClearestImgTop10(imgList);
+    TreeMap<String, Integer> frameFaceNumMap = new TreeMap<String, Integer>();
+    for (int k = 0; k < top10List.length; k++) {
+      int faceNum = detectFaceNum(top10List[k]);
+      frameFaceNumMap.put(top10List[k], faceNum);
+    }
+    ArrayList<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(frameFaceNumMap.entrySet());
+    Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+      public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+        return (o2.getValue()).compareTo(o1.getValue());
+      }
+    });
+    String[] chooseImg = new String[synopsisFrameNumEachVideoOrImg];
+    for (int j = 0; j < synopsisFrameNumEachVideoOrImg; j++) {
+      chooseImg[j] = list.get(j).getKey();
+    }
+    Arrays.sort(chooseImg);
+    for (int j = 0; j < synopsisFrameNumEachVideoOrImg; j++) {
+      outImg.metaData[frameFolderList.size()*synopsisFrameNumEachVideoOrImg + j].filePath = chooseImg[j];
+    }
+  }
+
   public static void main(String[] args)
   {
     if (args.length != 2) {
@@ -225,7 +342,10 @@ public class CreateSynopsis {
     String folderPath = args[0];
     String synopsisPath = args[1];
     CreateSynopsis cs = new CreateSynopsis(folderPath, synopsisPath);
-    cs.chooseSynopsisNaive();
+
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    //cs.chooseSynopsisNaive();
+    cs.chooseSynopsisClearestTop10DetectMostFace();
     cs.encodeOutImgLayoutNaive();
     cs.serializeOutImg();
     //cs.showIm(cs.outImg.img);
--- README.txt
+++ README.txt
@@ -1,10 +1,12 @@
 Compile:
-  javac CreateSynopsis.java
-  javac ExploreSynopsis.java
+  javac -cp ".:YOUR/PATH/TO/OPENCV/AR" CreateSynopsis.java
+  javac -Djava.library.path=YOUR/PATH/TO/OPENCVDYLIB -cp ".:YOUR/PATH/TO/OPENCVJAR" ExploreSynopsis.java
 
 Run:
   java CreateSynopsis YourMediaFolder synopsis.rgb
-  java -Xmx6144m synopsis.rgb
+  java -Xmx6144m ExploreSynopsis synopsis.rgb
 
 Notice:
-  Please set JVM max heap space as large as possible ( > 4G), because it buffered all video frames and images at once.
+  When compile and run CreateSynopsis, please link opencv path.
+
+  When run ExploreSynopsis, Please set JVM max heap space as large as possible ( > 4G), because it buffered all video frames and images at once.
